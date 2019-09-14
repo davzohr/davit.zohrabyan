374a7937e9ccd946669d2bb9a62007c2f0c7a5c8
davzohr <55139467+davzohr@users.noreply.github.com>
2019-09-15 01:11:39 +0300

    Update task_18.py

--- lab1/task_18.py
+++ lab1/task_18.py
@@ -5,32 +5,20 @@ from pyrob.api import *
 
 @task
 def task_8_28():
-	if wall_is_above():
-		while wall_is_above()and not wall_is_on_the_left():
-			move_left()
-		if wall_is_on_the_left():
-			if wall_is_above():
-				while wall_is_above():
-					move_right()
-				while not wall_is_above():
-					move_up()
-				while not wall_is_on_the_left():
-					move_left()
-			else:
-				while not wall_is_above():
-					move_up()
-				while not wall_is_on_the_left():
-					move_left()
-		else:
-			while not wall_is_above():
-				move_up()
-			while not wall_is_on_the_left():
-				move_left()					
-	else:
+	def f():
 		while not wall_is_above():
-			move_up()
+			move_up()	
 		while not wall_is_on_the_left():
-			move_left()
-
+			move_left()		
+	while not wall_is_on_the_left() and wall_is_above():
+		move_left()
+	if not wall_is_above():
+		f()
+	else:
+		while wall_is_above():
+			move_right()
+		f()		
+		
+		
 if __name__ == '__main__':
 	run_tasks()
