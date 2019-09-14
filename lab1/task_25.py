cef1272f3b452dbff95d39ccb3eaef7a7753c393
davzohr <55139467+davzohr@users.noreply.github.com>
2019-09-14 20:27:20 +0300

    Update task_25.py

--- lab1/task_25.py
+++ lab1/task_25.py
@@ -5,20 +5,7 @@ from pyrob.api import *
 
 @task
 def task_2_2():
-	move_down(n=2)
-	fill_cell()
-	for i in range(2):
-		move_right()
-		fill_cell()
-	move_left()
-	move_down()
-	fill_cell()
-	move_up(n=2)
-	fill_cell()
-	move_down()
-	move_right()
-	while not wall_is_on_the_right():
-		move_right(n=2)
+	def f():
 		fill_cell()
 		for i in range(2):
 			move_right()
@@ -30,6 +17,11 @@ def task_2_2():
 		fill_cell()
 		move_down()
 		move_right()
+	move_down(n=2)
+	f()
+	while not wall_is_on_the_right():
+		move_right(n=2)
+		f()
 	move_up()
 	move_left(n=2)				
 		
