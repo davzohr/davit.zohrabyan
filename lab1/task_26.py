711810b0505b9c9a3878c85d3e77c566f206754d
davzohr <55139467+davzohr@users.noreply.github.com>
2019-09-15 22:15:25 +0300

    Update task_26.py

--- lab1/task_26.py
+++ lab1/task_26.py
@@ -2,24 +2,22 @@
 
 from pyrob.api import *
 
+def cross():
+	fill_cell()
+	for i in range(2):
+		move_right()
+		fill_cell()
+	move_left()
+	move_down()
+	fill_cell()
+	move_up(n=2)
+	fill_cell()
+	move_down()
+	move_right()
+		
 
 @task(delay=0.02)
 def task_2_4():
-	
-	def cross():
-		fill_cell()
-		for i in range(2):
-			move_right()
-			fill_cell()
-		move_left()
-		move_down()
-		fill_cell()
-		move_up(n=2)
-		fill_cell()
-		move_down()
-		move_right()
-		
-
 	move_down(n=2)
 	while not wall_is_beneath():
 		move_up()
@@ -40,10 +38,6 @@ def task_2_4():
 		move_left()
 		
 			
-
-if __name__ == '__main__':
-	run_tasks()
-
 
 if __name__ == '__main__':
 	run_tasks()
