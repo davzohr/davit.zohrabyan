0c951c9e0a80df4b40869ee3ef2107aad2d8a14f
davzohr <55139467+davzohr@users.noreply.github.com>
2019-09-15 20:39:40 +0300

    Update task_19.py

--- lab1/task_19.py
+++ lab1/task_19.py
@@ -10,19 +10,15 @@ def task_8_29():
 			move_up()
 		while not wall_is_on_the_left():
 			move_left()
-	while wall_is_above() and not wall_is_on_the_right():
-		move_right()
+	while not wall_is_on_the_left():
+		move_left()
 	if not wall_is_above():
 		f()
 	else:
-		while not wall_is_on_the_left():
-			move_left()
+		while not wall_is_on_the_right():
+			move_right()
 		if not wall_is_above():
 			f()	
-		else:
-			while not wall_is_on_the_right():
-				move_right()
 			
-
 if __name__ == '__main__':
 	run_tasks()
