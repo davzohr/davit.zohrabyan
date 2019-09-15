403946d08e0833eb95417d241c0aaa25a48bafed
davzohr <55139467+davzohr@users.noreply.github.com>
2019-09-15 23:41:30 +0300

    Update task_30.py

--- lab1/task_30.py
+++ lab1/task_30.py
@@ -2,87 +2,39 @@
 
 from pyrob.api import *
 
+def left():
+	while not wall_is_on_the_left():
+		move_left()	
 
 @task(delay=0.01)
 def task_9_3():
 	a=1
-	while not wall_is_on_the_right():
-		move_right()
-		a=a+1
-	e=a/2+1
-	c=a-1
-	a=a-2
 	b=1
-	d=b
-	for i in range(a):
-		move_left()
-		fill_cell()
-	move_left()
-	while not c<e:
-		move_down()
-		for i in range(b):
-			fill_cell()
-			move_right()
-		move_right()
-		for i in range(a-2*b):
-			fill_cell()
-			move_right()
-		while not wall_is_on_the_right():
-			move_right()
-			fill_cell()
-		while not wall_is_on_the_left():
-			move_left()
-		c=c-1	
-		b=b+1
-	while not wall_is_beneath():
-		move_down()
-	a=1	
+	c=1
 	while not wall_is_on_the_right():
 		move_right()
-		a=a+1	
-	e=a/2+1
-	c=a-1
-	a=a-2
-	b=1
-	d=b
-	for i in range(a):
-		move_left()
-		fill_cell()
-	move_left()
-	while not c<e:
-		move_up()
-		for i in range(b):
-			fill_cell()
-			move_right()
-		move_right()
-		for i in range(a-2*b):
-			fill_cell()
-			move_right()
-		while not wall_is_on_the_right():
-			move_right()
-			fill_cell()
-		while not wall_is_on_the_left():
-			move_left()
-		c=c-1	
-		b=b+1
-	a=a+2
-	f=a//2
-	move_up()
-	for i in range(f):
-		fill_cell()
-		move_right()
-	while not wall_is_on_the_right():
-		move_right()
-		fill_cell()
-	while not wall_is_beneath():
+		a+=1
+	left()
+	while not wall_is_beneath():	
+		for i in range(a-1):
+			if c==b or c==(a-b+1):
+				move_right()
+				c+=1
+			else:
+				fill_cell()
+				move_right()
+				c+=1
+		b+=1
+		c=1	
+		if not b==2:
+			fill_cell()	
 		move_down()
-	while not wall_is_on_the_left():
-		move_left()				
-
-					
-			
-	 
-			
+		left()
+	for i in range(a-2):
+		move_right()
+		fill_cell()
+	left()
+		
 		
 		
 if __name__ == '__main__':
