c9778c913108cd12af3163ac6278058807629ec5
davzohr <55139467+davzohr@users.noreply.github.com>
2019-09-17 21:10:53 +0300

    Update task_8.py

--- lab2/task_8.py
+++ lab2/task_8.py
@@ -1,22 +1,22 @@
 import turtle as t
 from math import sin, pi
-n=3
-r=0
-def f():
+
+def polygon(r,n):
 	a=r*2*sin(pi/n)
 	for i in range(n):
 		t.forward(a)
 		t.left(360/n)
 
 t.shape('turtle')
-
+r=0
+n=3
 for i in range(10):
 	r+=30
 	t.penup()
 	t.forward(30)
 	t.pendown()
 	t.left(180-(n-2)*90/n)
-	f()
+	polygon(r,n)
 	t.right(180-(n-2)*90/n)
 	n+=1	
 input()
