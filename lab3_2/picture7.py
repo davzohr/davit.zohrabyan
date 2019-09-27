681404a8afb9439a2550fc0b4ec86f1cc09ace56
davzohr <55139467+davzohr@users.noreply.github.com>
2019-09-27 20:21:13 +0300

    Add files via upload

---
+++ lab3_2/picture7.py
@@ -0,0 +1,23 @@
+import graphics as gr
+
+win=gr.GraphWin('pic7_1',600,800)
+
+
+
+def main(win):
+    """draws picture"""
+    #draw_background(win)
+    #draw_sun()
+    #draw_black_cloud(win)
+    #draw_house(win)
+    #draw_pipes(win)
+    #draw_grey_clouds(win)
+    #draw_ghost(win)
+
+main(win)
+
+win.getMouse()
+win.close()
+
+
+
