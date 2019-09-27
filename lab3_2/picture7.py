0dda7043b772046950c7c60f0c576cd5013ff953
davzohr <55139467+davzohr@users.noreply.github.com>
2019-09-27 20:46:35 +0300

    Update picture7.py

--- lab3_2/picture7.py
+++ lab3_2/picture7.py
@@ -2,11 +2,18 @@ import graphics as gr
 
 win=gr.GraphWin('pic7_1',600,800)
 
-
+def draw_background(win):
+    ground = gr.Rectangle(gr.Point(0,400), gr.Point(600,800))
+    ground.draw(win)
+    ground.setFill('black')
+    
+    sky = gr.Rectangle(gr.Point(0,0), gr.Point(600,400))
+    sky.draw(win)
+    sky.setFill('lightgrey')	
 
 def main(win):
     """draws picture"""
-    #draw_background(win)
+    draw_background(win)
     #draw_sun()
     #draw_black_cloud(win)
     #draw_house(win)
@@ -18,6 +25,4 @@ main(win)
 
 win.getMouse()
 win.close()
-
-
 
