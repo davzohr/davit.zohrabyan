678c01e548e153ac626fc4d5b6c820aed7364db2
davzohr <55139467+davzohr@users.noreply.github.com>
2019-10-01 14:46:22 +0300

    Update picture7.py

--- lab3_2/picture7.py
+++ lab3_2/picture7.py
@@ -2,136 +2,145 @@ import graphics as gr
 
 win=gr.GraphWin('pic7_1',600,800)
 
+
 def draw_background(win):
-    ground = gr.Rectangle(gr.Point(0,400), gr.Point(600,800))
+    ground = gr.Rectangle(gr.Point(0, 400), gr.Point(600, 800))
     ground.draw(win)
     ground.setFill('black')
     
-    sky = gr.Rectangle(gr.Point(0,0), gr.Point(600,400))
+    sky = gr.Rectangle(gr.Point(0, 0), gr.Point(600, 400))
     sky.draw(win)
     sky.setFill('#999DA0')	
+        
 
 def draw_sun(win):
-    sun = gr.Circle(gr.Point(540,60),50)
+    sun = gr.Circle(gr.Point(540, 60), 50)
     sun.draw(win)
     sun.setFill('white')
+
     
 def draw_black_cloud(win):
-	black_cloud = gr.Oval(gr.Point(300,150), gr.Point(650,200))    	
-	black_cloud.draw(win)
-	black_cloud.setFill('black')
+    black_cloud = gr.Oval(gr.Point(300, 150), gr.Point(650, 200))    	
+    black_cloud.draw(win)
+    black_cloud.setFill('black')
+
 
 def draw_wall_and_windows(win):
-	wall = gr.Rectangle(gr.Point(20,150), gr.Point(350,600))
-	wall.draw(win)
-	wall.setFill('#4B3819')
-	
-	left_window = gr.Rectangle(gr.Point(60,470), gr.Point(120,550))
-	left_window.draw(win)
-	left_window.setFill('#492000')
-	
-	middle_window = gr.Rectangle(gr.Point(150,470), gr.Point(210,550))
-	middle_window.draw(win)
-	middle_window.setFill('#492000')
-	
-	right_window = gr.Rectangle(gr.Point(240,470), gr.Point(300,550))
-	right_window.draw(win)
-	right_window.setFill('#FFD801')
-	
-	x=42
-	for i in range(4):
-		window = gr.Rectangle(gr.Point(x,150), gr.Point(x+42,390))
-		window.draw(win)
-		window.setFill('#997950')
-		x+=84
+    wall = gr.Rectangle(gr.Point(20, 150), gr.Point(350, 600))
+    wall.draw(win)
+    wall.setFill('#4B3819')
+    
+    left_window = gr.Rectangle(gr.Point(60, 470), gr.Point(120, 550))
+    left_window.draw(win)
+    left_window.setFill('#492000')
+    
+    middle_window = gr.Rectangle(gr.Point(150, 470), gr.Point(210, 550))
+    middle_window.draw(win)
+    middle_window.setFill('#492000')
+    
+    right_window = gr.Rectangle(gr.Point(240, 470), gr.Point(300, 550))
+    right_window.draw(win)
+    right_window.setFill('#FFD801')
+    
+    x=42
+    for i in range(4):
+        window = gr.Rectangle(gr.Point(x, 150), gr.Point(x+42, 390))
+        window.draw(win)
+        window.setFill('#997950')
+        x+=84
+
 
 def draw_balcony(win):
-	base = gr.Rectangle(gr.Point(0,400), gr.Point(370,440))
-	base.draw(win)
-	base.setFill('#222021')			
-
-	left_column = gr.Rectangle(gr.Point(5,360), gr.Point(15,400))
-	left_column.draw(win)
-	left_column.setFill('#222021')		
-	
-	right_column = gr.Rectangle(gr.Point(355,360), gr.Point(365,400))
-	right_column.draw(win)
-	right_column.setFill('#222021')
-	
-	a=42
-	for i in range(5):
-		column = gr.Rectangle(gr.Point(a,360), gr.Point(a+22,400))
-		column.draw(win)
-		column.setFill('#222021')
-		a+=66		
+    base = gr.Rectangle(gr.Point(0, 400), gr.Point(370, 440))
+    base.draw(win)
+    base.setFill('#222021')			
     
-	railing = gr.Rectangle(gr.Point(15,360), gr.Point(355,340))
-	railing.draw(win)
-	railing.setFill('#222021')	
+    left_column = gr.Rectangle(gr.Point(5, 360), gr.Point(15, 400))
+    left_column.draw(win)
+    left_column.setFill('#222021')		
 	
+    right_column = gr.Rectangle(gr.Point(355, 360), gr.Point(365, 400))
+    right_column.draw(win)
+    right_column.setFill('#222021')
+    
+    a=42
+    for i in range(5):
+        column = gr.Rectangle(gr.Point(a, 360), gr.Point(a+22, 400))
+        column.draw(win)
+        column.setFill('#222021')
+        a+=66		
+    
+    railing = gr.Rectangle(gr.Point(15, 360), gr.Point(355, 340))
+    railing.draw(win)
+    railing.setFill('#222021')	
+
+
 def draw_roof(win):
-    roof = gr.Polygon(gr.Point(0,150), gr.Point(30,120), gr.Point(340,120), gr.Point(370,150))
+    roof = gr.Polygon(gr.Point(0, 150), gr.Point(30, 120), gr.Point(340, 120), gr.Point(370, 150))
     roof.draw(win)
     roof.setFill('black')
     
-    pipe = gr.Rectangle(gr.Point(60,80), gr.Point(70,135))
+    pipe = gr.Rectangle(gr.Point(60, 80), gr.Point(70, 135))
     pipe.draw(win)
     pipe.setFill('#222021')
     
-    pipe = gr.Rectangle(gr.Point(200,80), gr.Point(210,120))
+    pipe = gr.Rectangle(gr.Point(200, 80), gr.Point(210, 120))
     pipe.draw(win)
     pipe.setFill('#222021')
-    
+
+
 def draw_clouds(win):
-	cloud = gr.Oval(gr.Point(20,100), gr.Point(500,50))
-	cloud.draw(win)
-	cloud.setFill('#48494B')
-	
-	cloud = gr.Oval(gr.Point(280,80), gr.Point(610,25))
-	cloud.draw(win)
-	cloud.setFill('#777B7E')
-	
-	cloud = gr.Oval(gr.Point(370,140), gr.Point(800,100))  
-	cloud.draw(win)
-	cloud.setFill('#777B7E')  
+    cloud = gr.Oval(gr.Point(20, 100), gr.Point(500, 50))
+    cloud.draw(win)
+    cloud.setFill('#48494B')
     
+    cloud = gr.Oval(gr.Point(280, 80), gr.Point(610, 25))
+    cloud.draw(win)
+    cloud.setFill('#777B7E')
+    
+    cloud = gr.Oval(gr.Point(370, 140), gr.Point(800, 100))  
+    cloud.draw(win)
+    cloud.setFill('#777B7E')  
+
+
 def draw_pipes(win):
-	pipe = gr.Rectangle(gr.Point(80,50), gr.Point(100,140))
-	pipe.draw(win)
-	pipe.setFill('#222021')
-	
-	pipe = gr.Rectangle(gr.Point(300,80), gr.Point(310,135))
-	pipe.draw(win)
-	pipe.setFill('#222021')
-	
+    pipe = gr.Rectangle(gr.Point(80, 50), gr.Point(100, 140))
+    pipe.draw(win)
+    pipe.setFill('#222021')
+    
+    pipe = gr.Rectangle(gr.Point(300, 80), gr.Point(310, 135))
+    pipe.draw(win)
+    pipe.setFill('#222021')
+
+
 def draw_ghost(win):
 	
-	body = gr.Polygon(gr.Point(460,615), gr.Point(450,645), gr.Point(410,700)
-	, gr.Point(390,730), gr.Point(435,720), gr.Point(445,740), gr.Point(490,715)
-	, gr.Point(525,735), gr.Point(550,710), gr.Point(540,670), gr.Point(510,640), gr.Point(500,600))
-	body.draw(win)
-	body.setFill('silver')
-	
-	head = gr.Circle(gr.Point(475,600),25)
-	head.draw(win)
-	head.setFill('silver')	    
+    body = gr.Polygon(gr.Point(460, 615), gr.Point(450, 645), gr.Point(410, 700)
+    , gr.Point(390, 730), gr.Point(435, 720), gr.Point(445, 740), gr.Point(490, 715)
+    , gr.Point(525, 735), gr.Point(550, 710), gr.Point(540, 670), gr.Point(510, 640), gr.Point(500, 600))
+    body.draw(win)
+    body.setFill('silver')
     
-	eye = gr.Circle(gr.Point(462,602),6)
-	eye.draw(win)
-	eye.setFill('lightblue')
+    head = gr.Circle(gr.Point(475, 600), 25)
+    head.draw(win)
+    head.setFill('silver')	    
+    
+    eye = gr.Circle(gr.Point(462, 602), 6)
+    eye.draw(win)
+    eye.setFill('lightblue')
 	
-	eye = gr.Circle(gr.Point(488,598),6)
-	eye.draw(win)
-	eye.setFill('lightblue')
-	
-	pupil = gr.Circle(gr.Point(461,603),2)
-	pupil.draw(win)
-	pupil.setFill('black')
-	
-	pupil = gr.Circle(gr.Point(487,599),2)
-	pupil.draw(win)
-	pupil.setFill('black')
-			 		 
+    eye = gr.Circle(gr.Point(488, 598), 6)
+    eye.draw(win)
+    eye.setFill('lightblue')
+    
+    pupil = gr.Circle(gr.Point(461, 603), 2)
+    pupil.draw(win)
+    pupil.setFill('black')
+    
+    pupil = gr.Circle(gr.Point(487, 599), 2)
+    pupil.draw(win)
+    pupil.setFill('black')
+    		 		 
     
 def main(win):
     """draws picture"""
@@ -149,6 +158,7 @@ main(win)
 
 win.getMouse()
 win.close()
+
 
 
 
