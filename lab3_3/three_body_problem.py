a3ac3f2cb293397dc2c3f8566d0b22489a3568fe
davzohr <55139467+davzohr@users.noreply.github.com>
2019-10-08 21:45:40 +0300

    Update three_body_problem.py

--- lab3_3/three_body_problem.py
+++ lab3_3/three_body_problem.py
@@ -1,7 +1,11 @@
 import graphics as gr
 window = gr.GraphWin("three body problem", 600, 600)
 
-dt=0.0005
+a=0
+
+"""importing characteristics of bodies"""
+
+dt=0.0001
 G=2000
 
 m1=10000
@@ -23,7 +27,7 @@ v1y=-300
 v2x=-300
 v2y=300
 
-v3x=50
+v3x=0
 v3y=0
 
 """drawing planets"""
@@ -42,84 +46,92 @@ pl3.setFill('green')
 
 """equation_of_motion"""
 for i in range (1000000):
-	d12 = ((x1-x2)**2+(y1-y2)**2)**0.5
-	d13 = ((x1-x3)**2+(y1-y3)**2)**0.5
-	d23 = ((x2-x3)**2+(y2-y3)**2)**0.5
+    d12 = ((x1-x2) ** 2 + (y1-y2) ** 2) ** 0.5
+    d13 = ((x1-x3) ** 2 + (y1-y3) ** 2) ** 0.5
+    d23 = ((x2-x3) ** 2 + (y2-y3) ** 2) ** 0.5
+    
+    """force_laws"""		
+    force12x = G * m1 * m2 / d12**2 * (x2-x1) / d12
+    force12y = G * m1 * m2 / d12**2 * (y2-y1) / d12
+
+    force13x = G * m1 * m3 / d13**2 * (x3-x1) / d13
+    force13y = G * m1 * m3 / d13**2 * (y3-y1) / d13
+
+    force21x = G * m1 * m2 / d12**2 * (x1-x2) / d12
+    force21y = G * m1 * m2 / d12**2 * (y1-y2) / d12
+
+    force23x = G * m2 * m3 / d23**2 * (x3-x2) / d23
+    force23y = G * m2 * m3 / d23**2 * (y3-y2) / d23
+
+    force31x = G * m3 * m1 / d13**2 * (x1-x3) / d13
+    force31y = G * m3 * m1 / d13**2 * (y1-y3) / d13
+
+    force32x = G * m3 * m2 / d23**2 * (x2-x3) / d23
+    force32y = G * m3 * m2 / d23**2 * (y2-y3) / d23	
+
+    """acceleration changing law"""	
+    a1x = (force12x + force13x) / m1
+    a1y = (force12y + force13y) / m1
+
+    a2x = (force21x + force23x) / m2
+    a2y = (force21y + force23y) / m2
+	
+    a3x = (force31x + force32x) / m3
+    a3y = (force31y + force32y) / m3
+
+    """coordinate changing laws"""
+    x1 = x1 + (v1x * dt)
+    y1 = y1 + (v1y * dt)
+
+    x2 = x2 + (v2x * dt)
+    y2 = y2 + (v2y * dt)
+
+    x3 = x3 + (v3x * dt)
+    y3 = y3 + (v3y * dt)
+
+    dx1 = v1x * dt
+    dy1 = v1y * dt
+
+    dx2 = v2x * dt
+    dy2 = v2y * dt
+	
+    dx3 = v3x * dt
+    dy3 = v3y * dt
+	
+    pl1.move(dx1,dy1)
+
+    pl2.move(dx2,dy2)
+	
+    pl3.move(dx3,dy3)
+
+    """velocity_changing_laws"""		
+    v1x = v1x + (a1x * dt)
+    v1y = v1y + (a1y * dt)	
+
+    v2x = v2x + (a2x * dt)
+    v2y = v2y + (a2y * dt)	
+	
+    v3x = v3x + (a3x * dt)
+    v3y = v3y + (a3y * dt)
+
+    """trajectoies of planets"""
+    if a%100==0:
+    	trajectory1 = gr.Point(x1,y1)
+     	trajectory2 = gr.Point(x2,y2)
+    	trajectory3 = gr.Point(x3,y3)
 		
-	force12x = G * m1 * m2 / d12**2 * (x2-x1) / d12
-	force12y = G * m1 * m2 / d12**2 * (y2-y1) / d12
-
-	force13x = G * m1 * m3 / d13**2 * (x3-x1) / d13
-	force13y = G * m1 * m3 / d13**2 * (y3-y1) / d13
-
-	force21x = G * m1 * m2 / d12**2 * (x1-x2) / d12
-	force21y = G * m1 * m2 / d12**2 * (y1-y2) / d12
-
-	force23x = G * m2 * m3 / d23**2 * (x3-x2) / d23
-	force23y = G * m2 * m3 / d23**2 * (y3-y2) / d23
-
-	force31x = G * m3 * m1 / d13**2 * (x1-x3) / d13
-	force31y = G * m3 * m1 / d13**2 * (y1-y3) / d13
-	
-	force32x = G * m3 * m2 / d23**2 * (x2-x3) / d23
-	force32y = G * m3 * m2 / d23**2 * (y2-y3) / d23	
-	
-	a1x = (force12x + force13x) / m1
-	a1y = (force12y + force13y) / m1
-
-	a2x = (force21x + force23x) / m2
-	a2y = (force21y + force23y) / m2
-	
-	a3x = (force31x + force32x) / m3
-	a3y = (force31y + force32y) / m3
-
-	x1 = x1 + (v1x * dt)
-	y1 = y1 + (v1y * dt)
-
-	x2 = x2 + (v2x * dt)
-	y2 = y2 + (v2y * dt)
-	
-	x3 = x3 + (v3x * dt)
-	y3 = y3 + (v3y * dt)
-
-	dx1 = v1x * dt
-	dy1 = v1y * dt
-
-	dx2 = v2x * dt
-	dy2 = v2y * dt
-	
-	dx3 = v3x * dt
-	dy3 = v3y * dt
-	
-	pl1.move(dx1,dy1)
-
-	pl2.move(dx2,dy2)
-	
-	pl3.move(dx3,dy3)
+	trajectory1.draw(window)
+	trajectory2.draw(window)
+	trajectory3.draw(window)
 		
-	v1x = v1x + (a1x * dt)
-	v1y = v1y + (a1y * dt)	
-
-	v2x = v2x + (a2x * dt)
-	v2y = v2y + (a2y * dt)	
-	
-	v3x = v3x + (a3x * dt)
-	v3y = v3y + (a3y * dt)
-	
-#	trajectory1 = gr.Point(x1,y1)
-#	trajectory2 = gr.Point(x2,y2)
-#	trajectory3 = gr.Point(x3,y3)
-	
-#	trajectory1.draw(window)
-#	trajectory2.draw(window)
-#	trajectory3.draw(window)
-	
-#	trajectory1.setFill('red')
-#	trajectory2.setFill('blue')
-#	trajectory3.setFill('green')
+	trajectory1.setFill('red')
+	trajectory2.setFill('blue')
+	trajectory3.setFill('green')
+	a+=1	
+    else:
+	a+=1	
 		
-
 window.getMouse()
 window.close()
-input() 
 
+	
